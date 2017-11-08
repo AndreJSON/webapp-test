afdf0ac89972582a190d56ec10da259b828c8f1c
AndreJSON <andre.v.json@gmail.com>
2017-11-08 14:32:17 +0100

    Cleanup.

--- public/scripts/controllers/main-controller.js
+++ public/scripts/controllers/main-controller.js
@@ -1,23 +1,10 @@
 /*global angular*/
 
-angular.module('app').controller('mainController', function ($scope, $log, $http, $timeout) {
+angular.module('app').controller('mainController', function ($scope, $log, $timeout) {
 	'use strict';
 	
-	$scope.greeting = undefined;
-	
-	function getGreeting() {
-		$http.get('/api/greeting', {params: {name: 'Mr. World'}}).then(
-			function (res) {
-				$scope.greeting = res.data.greeting;
-			},
-			function (err) {
-				$log.info('Could not get greeting.');
-			}
-		);
-	}
-	
 	function init() {
-		getGreeting();
+		$log.info("Hello world!");
 	}
 	
 	$timeout(init);
