64aba02c2bd79151d1192fb84c03202f43d70098
AndreJSON <andre.v.json@gmail.com>
2017-11-09 22:06:47 +0100

    Add some more dummy data to make it look nicer.

--- public/scripts/controllers/rewards-controller.js
+++ public/scripts/controllers/rewards-controller.js
@@ -9,8 +9,20 @@ angular.module('app').controller('rewardsController', function ($scope, $mdDialo
 			what: "Free Coffee at Nymble"
 		},
 		{
+			points: 75,
+			what: "10kr discount at restaurant Q"
+		},
+		{
 			points: 100,
 			what: "Sandwich for 5kr at KTHB"
+		},
+		{
+			points: 200,
+			what: "15kr discount at Syster o Bror"
+		},
+		{
+			points: 500,
+			what: "10% off one purchase at Kårbokhandeln"
 		}
 	];
 });
